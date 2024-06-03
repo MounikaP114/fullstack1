2987160b49ee25d1d71efd6216b84fc291f84120
Mounika P <mounikapotladurthiofficial@gmail.com>
2024-06-03 09:56:24 +0530

    dummytasks added statically

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import ToDo from './components/ToDo';
+import './index.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='container'>
+        <h1>To Do App</h1>
+        <div className='top'>
+          <input type="text" value='text' placeholder="add todo task"></input>
+          <button className='button'>Add Task</button>
+        </div>
+        <div className='list'>
+        </div>
+          <ToDo className='' text={"Hi"}></ToDo>
+          <ToDo className='' text={"Hi"}></ToDo>
+          <ToDo className='' text={"Hi"}></ToDo>
+          <ToDo className='' text={"Hi"}></ToDo>
+
+        </div>   
     </div>
   );
 }
